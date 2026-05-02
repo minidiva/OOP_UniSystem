b295bb6682ae8b10baab4d2d3f3f7f654693f89b
Alan <ala686876@gmail.com>
2026-05-02 13:15:39 +0500

    Added example course

--- StudentPortal/src/app/App.java
+++ StudentPortal/src/app/App.java
@@ -2,6 +2,7 @@ package app;
 import java.util.*;
 import util.Printer;
 import command.core.*;
+import domain.course.Course;
 import domain.user.Student;
 import domain.user.User;
 import repository.CourseRepository;
@@ -20,6 +21,8 @@ public class App {
         CommandRegistry registry = new CommandRegistry();
         CommandManager manager = new CommandManager();
         CourseRepository courseRepository = new CourseRepository();
+
+        courseRepository.save(new Course(1, "Introduction to Networks", "Introductory course for freshmen", 6));
 
         User user = new Student();
         Menu menu = new Menu(registry, printer);
